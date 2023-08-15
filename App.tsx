ccb93c700b9c1141cfb8f498e6050f3f56102b9d
Whit3Demon <koliakolmikov@gmail.com>
2023-08-15 21:27:28 +0300

    fix Platforms bars color

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createAppContainer } from "react-navigation";
-import { StatusBar } from "react-native";
+import { Platform, StatusBar } from "react-native";
 import AppNavigator from "./src/screens/navigation/AppNavigator";
 
 export default function App() {
-  StatusBar.setBarStyle("dark-content");
+  //StatusBar.setBarStyle("dark-content");
   const AppContainer = createAppContainer(AppNavigator);
   return (
     <NavigationContainer>
+      <StatusBar
+        barStyle={Platform.OS === "ios" ? "dark-content" : "light-content"}
+      />
       <AppContainer />
     </NavigationContainer>
   );
